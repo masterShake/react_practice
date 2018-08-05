3554ef2149e05c5977e2766f6d31f5bc4cd8e50c
Michael Giannini <therealmichaelgiannini@gmail.com>
2018-08-05 14:15:40 -0500

    finished countdown champ tutorial

--- countdown-champ/src/App.jsx
+++ countdown-champ/src/App.jsx
@@ -1,9 +1,40 @@
 import React, { Component } from 'react'
+import Clock from './Clock'
+import { Form, FormControl, Button } from 'react-bootstrap'
+import './App.css'
 
 class App extends Component{
+
+	constructor(props){
+		super(props)
+		this.state = {
+			deadline : 'July 25, 2019',
+			newDeadline : ''
+		}
+	}
+
+	changeDeadline(){
+		this.setState({deadline : this.state.newDeadline})
+	}
+
 	render(){
 		return(
-			<div>Countdown Champ, App Component</div>
+			<div className="App">
+				<h1 className="App-title">Coundown to {this.state.deadline}</h1>
+				<Clock
+					deadline={this.state.deadline}
+				/>
+				<Form inline>
+					<FormControl 
+						className="Deadline-input"
+						placeholder='new date'
+						onChange={event => this.setState({newDeadline : event.target.value})}
+					/>
+					<Button
+						onClick={() => this.changeDeadline()}
+					>Submit</Button>
+				</Form>
+			</div>
 		)
 	}
 }
